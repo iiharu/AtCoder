b9f0c2197455f6826d183add08b1f46ff6a83f10
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-05 23:36:07 +0900

    [ARC 004] A - The longest distance

--- main.cpp
+++ main.cpp
@@ -7,25 +7,34 @@ struct Fast {Fast() {cin.tie(0); ios::sync_with_stdio(false);}} fast;
 
 
 signed main(void) {
-  /* */
-  int num; // num
-  num = 0; // count, result
+  // int num; // num
+  // num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
-  num = numeric_limits<int>::max(); // min
+  // num = numeric_limits<int>::max(); // min
+  double num;
+  num = numeric_limits<double>::min();
   int N;
   // int M;
-  array<int, 10000> A;
+  // array<int, 10000> A;
+  array<pair<int, int>, 100> A;
   string S;
   // string T;
-  /* */
   
   cin >> N;
-  for (auto it = A.begin(); it < A.begin() + N; ++it) cin >> *it;
 
-  for (auto it = A.begin(); it < A.begin() + N; ++it)
-    cout << it - A.begin() << " :" << *it << endl;
+  for (auto it = A.begin(); it < A.begin() + N; ++it) {
+    int x, y;
+    cin >> x >> y;
+    *it = make_pair(x, y);
+  }
 
-  
+  for (auto it = A.begin(); it < A.begin() + N; ++it) {
+    for (auto it1 = A.begin(); it1 < A.begin() + N; ++it1) {
+      double dist = sqrt(pow(((*it).first - (*it1).first), 2) + pow(((*it).second - (*it1).second), 2));
+      num = max(num, dist);
+    }
+  }
+
   cout << num << endl;
   return 0;
 }
