205c19af874bcf5e24023f2f416c52fd8b991865
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-06 20:54:29 +0900

    [AtCoder ARC 029] A - 高橋くんとお肉

--- main.cpp
+++ main.cpp
@@ -7,44 +7,34 @@ struct Fast {Fast() {cin.tie(0); ios::sync_with_stdio(false);}} fast;
 
 signed main(void) {
   int num; // num
-  num = 0; // count, result
+  // num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
-  // num = numeric_limits<int>::max(); // min
-  // int N;
+  num = numeric_limits<int>::max(); // min
+  int N;
   // int M;
   array<int, 4> A;
-  string S;
+  // string S;
   // string T;
 
-  cin >> S;
+  cin >> N;
+  for (int i = 0; i < N; ++i) cin >> A[i];
 
-  for (int i = 0; i < 4; ++i) {
-    A[i] = (int)(S[i] - '0');
-  }
+  for (int i = 0; i < pow(2, N); ++i) {
+    int t1 = 0;
+    int t2 = 0;
 
-  for (int i = 0; i < pow(2, 3); ++i) {
-    int tmp = A[0];
-    bitset<3> bs(i);
-
-    for (int j = 0; j < 3; j++) {
-      if (bs.test(j)) tmp += A[j + 1];
-      else tmp -= A[j + 1];
+    bitset<4> bs(i);
+    
+    for (int j = 0; j < N; ++j) {
+      if(bs.test(j)) t1 += A[j];
+      else t2 += A[j];
     }
 
-    if (tmp == 7) {
-      cout << A[0];
-      for (int j = 0; j < 3; j++) {
-        if (bs.test(j)) cout << "+";
-        else cout << "-";
-        cout << A[j + 1];
-      }
-      cout << "=7" << endl;
-      return 0;
-    }
+    num = min(num, max(t1, t2));
     
   }
   
-  // cout << num << endl;
+  cout << num << endl;
   // cout << fixed << setprecision(10) << num << endl;
   return 0;
 }
