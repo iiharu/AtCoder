86b88c027f3293daa1b9a2a28828b7f5444104b8
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-06 20:29:42 +0900

    Update README.org and bits/stdc++.h

--- bits/stdc++.h
+++ bits/stdc++.h
@@ -3,18 +3,12 @@
 #include <array>
 #include <bitset>
 #include <cmath>
-/* #include <deque> */
 #include <iomanip>
 #include <iostream>
-/* #include <iterator> */
 #include <limits>
-/* #include <list> */
 #include <map>
 #include <numeric>
-/* #include <queue> */
 #include <set>
-/* #include <stack> */
 #include <string>
-/* #include <tuple> */
 #include <utility>
 #include <vector>
