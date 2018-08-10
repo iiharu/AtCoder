185cd3e57ed790f8090178dd4038af5d98d1d5c0
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-10 19:17:07 +0900

    Revert main.cpp

--- main.cpp
+++ main.cpp
@@ -12,53 +12,18 @@ signed main(void) {
   // num = numeric_limits<int>::max(); // min
   int N;
   // int M;
-  // array<int, 10000> A;
+  array<int, 10000> A;
   // string S;
   // string T;
 
   cin >> N;
 
-  vector<pair<int, int>> Q;
+  for (auto it = A.begin(); it < A.begin() + N; ++it)
+    cin >> *it;
 
-  do {
-    bool isQueen = true;
+  for (auto it = A.begin(); it < A.begin() + N; ++it)
+    if (*it % 2 == 0) ++num;
 
-    if (Q.size() > 0) {
-      for (auto it0 = Q.begin(); it0 < Q.end(); ++it0) {
-        pair<int, int> p = *it0;
-        for (auto it1 = it0 + 1; it1 < Q.end(); ++it1) {
-          pair<int, int> q = *it1;
-          if (p.first == q.first) isQueen = false;
-          if (p.second == q.second) isQueen = false;
-          if (abs(p.first - q.first) == abs(p.second - q.second)) isQueen = false;
-          if (!isQueen) break;
-        }
-        if (!isQueen) break;
-      }
-
-      if (isQueen && (int)Q.size() == N) {
-        for (pair<int, int> p : Q) cout << "(" << p.first << ", " << p.second << ")" << " ";
-        cout << endl;
-        ++num;
-      }
-    }
-
-    if (isQueen && (int) Q.size() < N) Q.push_back(make_pair(Q.size() + 1, 1));
-    else {
-      do {
-        pair<int, int> &p = Q.back();
-        pair<int, int> q = make_pair(p.first, p.second);
-        Q.pop_back();
-
-        if (q.second < N) {
-          q.second = q.second + 1;
-          Q.push_back(q);
-          break;
-        }       
-      } while (!Q.empty());
-    }
-  } while (!Q.empty());
-    
   cout << num << endl;
   
   return 0;
