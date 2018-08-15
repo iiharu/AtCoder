13870cf5fb4b3b5caa36d6364227bfe51923967a
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-16 00:39:29 +0900

    Update

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 #define int long long
-struct Fast {Fast() {cin.tie(0); ios::sync_with_stdio(false);}} fast;
 
 signed main(void) {
   // bool flag = true;
@@ -12,21 +11,16 @@ signed main(void) {
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
   int N;
-  // int M;
+  int M;
   array<int, 10000> A;
-  // string yes = "Yes";
-  // string no = "No";
   // string S;
   // string T;
-  
+
   cin >> N;
-  
   for (auto it = A.begin(); it < A.begin() + N; ++it)
     cin >> *it;
-
   for (auto it = A.begin(); it < A.begin() + N; ++it)
     if (*it % 2 == 0) ++num;
-
   cout << num << endl;
   
   return 0;
