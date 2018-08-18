425196c8d90e474524c0e16990feaf562efcdf85
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-18 22:55:32 +0900

    [AtCoder ABC 106] B - 105

--- main.cpp
+++ main.cpp
@@ -9,17 +9,22 @@ signed main(void) {
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
-  // int N;
+  int N;
   // int M;
   // array<int, 10000> A;
   // string S;
   // string T;
 
-  int A, B;
-  cin >> A;
-  cin >> B;
+  cin >> N;
+  for (int i = 1; i <= N; i += 2) {
+    int tmp = 0;
+    for (int j = 1; j <= i; ++j) {
+      if (i % j == 0) ++tmp;
+    }
+    if (tmp == 8) ++num;
+  }
 
-  cout << (A - 1) * (B - 1) << endl;
+  cout << num << endl;
   
   return 0;
 }
