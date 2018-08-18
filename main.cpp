9a958bf6c6b94d874d7fd7941ccaa31e033e2968
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-18 23:01:20 +0900

    Fix confilict

--- main.cpp
+++ main.cpp
@@ -5,35 +5,12 @@ using namespace std;
 #define int long long
 
 signed main(void) {
-<<<<<<< HEAD
-=======
-  // bool flag = true;
->>>>>>> 13870cf5fb4b3b5caa36d6364227bfe51923967a
   int num; // num
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
-<<<<<<< HEAD
-  // int N;
-  // int M;
-  // array<int, 10000> A;
-  string S;
-  // string T;
-
-  int K;
-  cin >> S;
-  cin >> K;
-
-  for (auto it = S.begin(); it < S.end(); ++it) {
-    K -= pow(*it - '0', 5000000000000000);
-    if (K <= 0) {
-      cout << *it << endl;
-      break;
-    }
-  }
-=======
   int N;
-  int M;
+  // int M;
   array<int, 10000> A;
   // string S;
   // string T;
@@ -44,7 +21,6 @@ signed main(void) {
   for (auto it = A.begin(); it < A.begin() + N; ++it)
     if (*it % 2 == 0) ++num;
   cout << num << endl;
->>>>>>> 13870cf5fb4b3b5caa36d6364227bfe51923967a
   
   return 0;
 }
