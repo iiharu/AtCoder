8e4fe9534d128cb41e98a6b59b24e5c46259c049
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-25 22:39:15 +0900

    Revert main.cpp

--- main.cpp
+++ main.cpp
@@ -11,79 +11,16 @@ signed main(void) {
   // num = numeric_limits<int>::max(); // min
   int N;
   // int M;
-  // array<int, 10000> A;
+  array<int, 10000> A;
   // string S;
   // string T;
-
-  int H, W;
-
-  array<array<char, 100>, 100> A;
-
-  cin >> H >> W;
-  for (int i = 0; i < H; ++i) for (int j = 0; j < W; ++j) cin >> A[i][j];
-
   
-  bool changed = true;
-
-  while (changed) {
-    changed = false;
-
-    for (int i = 0; i < H; ++i) {
-      bool flag = true;
-      
-      for (int j = 0; j < W; ++j) if (A[i][j] == '#') flag = false;
-      
-      if (flag) {
-        flag = false;
-        for (int j = 0; j < W; ++j) if (A[i][j] == '.') flag = true;
-      }
-
-      if (flag) {
-        for (int j = 0; j < W; ++j) A[i][j] = ' ';
-        changed = true;
-        break;
-      } 
-    }
-
-    if (changed) continue;
-
-    for (int j = 0; j < W; ++j) {
-      bool flag = true;
-
-      for (int i = 0; i < H; ++i) if (A[i][j] == '#') flag = false;
-
-      if (flag) {
-        flag = false;
-        for (int i = 0; i < H; ++i) if (A[i][j] == '.') flag = true;
-      }
-
-      if (flag) {
-        for (int i = 0; i < H; ++i) A[i][j] = ' ';
-        changed = true;
-        break;
-      }
-    }
-
-    if (changed) continue;
-  }
-  
-  for (int i = 0; i < H; ++i) {
-    int n = 0;
-    for (int j = 0; j < W; ++j) {
-      if (A[i][j] != ' ') {
-        cout << A[i][j];
-        ++n;
-      }
-    }
-    if (n > 0) cout << endl;
-  }
-  
-  // cin >> N;
-  // for (auto it = A.begin(); it < A.begin() + N; ++it)
-  //   cin >> *it;
-  // for (auto it = A.begin(); it < A.begin() + N; ++it)
-  //   if (*it % 2 == 0) ++num;
-  // cout << num << endl;
+  cin >> N;
+  for (auto it = A.begin(); it < A.begin() + N; ++it)
+    cin >> *it;
+  for (auto it = A.begin(); it < A.begin() + N; ++it)
+    if (*it % 2 == 0) ++num;
+  cout << num << endl;
   
   return 0;
 }
