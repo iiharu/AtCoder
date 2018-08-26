f7125b76186f0df9edc555f1a97ec1399fb81476
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-26 14:56:42 +0900

    Update

--- main.cpp
+++ main.cpp
@@ -6,23 +6,20 @@ using namespace std;
 
 signed main(void) {
   int num; // num
-  // num = 0; // count, result
+  num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
-  num = numeric_limits<int>::max(); // min
+  // num = numeric_limits<int>::max(); // min
   int N;
-  int M;
+  // int M;
   array<int, 1000000> A;
   // string S;
   // string T;
 
-  cin >> N >> M;
-  for (auto it = A.begin(); it < A.begin() + N; ++it) cin >> *it;
-
-  for (int i = 0; i < N - M + 1; ++i) {
-    int tmp = min(abs(A[i]) + abs(A[i] - A[i + M - 1]),
-                  abs(A[i + M - 1]) + abs(A[i] - A[i + M - 1]));
-    num = min(num, tmp);
-  }
+  cin >> N;
+  for (auto it = A.begin(); it < A.begin() + N; ++it)
+    cin >> *it;
+  for (auto it = A.begin(); it < A.begin() + N; ++it)
+    if (*it % 2 == 0) ++num;
   cout << num << endl;
   
   return 0;
