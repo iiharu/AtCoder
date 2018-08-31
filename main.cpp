5e717612d0fc413e8c4308ca585f856e9af45fde
iiharu <8288781+iiharu@users.noreply.github.com>
2018-08-31 19:48:21 +0900

    Revert main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-// #define int long long
+#define int long long
 
 signed main(void) {
   int num; // num
@@ -11,50 +11,16 @@ signed main(void) {
   // num = numeric_limits<int>::max(); // min
   int N;
   // int M;
-  array<int, 1000000> A;
+  array<int, 1000000> A;;
   // string S;
   // string T;
-
-  int S;
-  int T;
-
-  // vector<int> N;
-  deque<pair<int, int>> tree;
   
-  cin >> N >> S >> T;
-
-  num = -1;
-  
-  
-  if (S == T) {
-    num = 0;
-  } else if (S > T) {
-    num = -1;
-  } else {
-    tree.push_back(make_pair(0, S));
-    int treeMax = pow(2, N);
-    
-    while (!tree.empty()) {
-      pair<int, int> n = tree.front();
-      tree.pop_front();
-      if (n.second == T) {
-        num = n.first;
-        break;
-      }
-      if (n.second * 2 < treeMax) tree.push_back(make_pair(n.first + 1, n.second * 2));
-      if (n.second * 2 + 1< treeMax) tree.push_back(make_pair(n.first + 1, n.second * 2 + 1));
-    }
-  }
-  
-
+  cin >> N;
+  for (auto it = A.begin(); it < A.begin() + N; ++it)
+    cin >> *it;
+  for (auto it = A.begin(); it < A.begin() + N; ++it)
+    if (*it % 2 == 0) ++num;
   cout << num << endl;
-  
-  // cin >> N;
-  // for (auto it = A.begin(); it < A.begin() + N; ++it)
-  //   cin >> *it;
-  // for (auto it = A.begin(); it < A.begin() + N; ++it)
-  //   if (*it % 2 == 0) ++num;
-  // cout << num << endl;
   
   return 0;
 }
