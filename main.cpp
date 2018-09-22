9ed949358b713914e572d05a34ede61c3d279204
iiharu <8288781+iiharu@users.noreply.github.com>
2018-09-22 21:53:04 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,40 +17,18 @@ signed main(void) {
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
   int N;
-  int M;
-  // array<int, 100000> A;
+  // int M;
+  // int K;
+  array<int, 100000> A;
   // string S;
   // string T;
-  int A, B, C;
-  // array<int, 100> O;
-  array<int, 100> L;
-  array<int, 100> R;
-  array<bool, 100> flag;
 
-  for (int i = 0; i < 100; ++i) flag[i] = false;
-  
-  cin >> N >> M >> A >> B;
+  cin >> N;
+  for (auto it = A.begin(); it < A.begin() + N; ++it) cin >> *it;
 
-  for (int i = 0; i < M; i++) {
-    int l, r;
-    cin >> l >> r;
-    for (int j = l - 1; j <= r - 1; ++j) flag[j] = true;
-  }
+  for (auto it = A.begin(); it < A.begin() + N; ++it) if (*it % 2 == 0) ++num;
 
-  C = max(A, B);
-  for (int i = 0; i < N; ++i) {
-    if (flag[i]) num += A;
-    else num += C;
-  }
-
-  
   cout << num << endl;
-  // cin >> N;
-  // for (auto it = A.begin(); it < A.begin() + N; ++it) cin >> *it;
-
-  // for (auto it = A.begin(); it < A.begin() + N; ++it) if (*it % 2 == 0) ++num;
-
-  // cout << num << endl;
   
   return 0;
 }
