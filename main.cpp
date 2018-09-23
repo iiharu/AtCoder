b003245790f02c00fe79b2ce75f2d712a26a4bdb
iiharu <8288781+iiharu@users.noreply.github.com>
2018-09-23 22:23:56 +0900

    Revert main.cpp

--- main.cpp
+++ main.cpp
@@ -17,48 +17,17 @@ signed main(void) {
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
   int N;
-  int M;
+  // int M;
   array<int, 100000> A;;
   // string S;
   // string T;
 
-  int X, Y;
+  cin >> N;
+  for (auto it = A.begin(); it < A.begin() + N; ++it) cin >> *it;
 
-  cin >> N >> M >> X >> Y;
+  for (auto it = A.begin(); it < A.begin() + N; ++it) if (*it % 2 == 0) ++num;
 
-  int XM, Ym;
-
-  for (int i = 0; i < N; ++i) {
-    int x;
-    cin >> x;
-    if (i == 0) XM = x;
-    else XM = max(XM, x);
-  }
-
-  for (int j = 0; j < M; j++) {
-    int y;
-    cin >> y;
-    if (j == 0) Ym = y;
-    else Ym = min(Ym, y);
-  }
-
-  bool flag = false;
-  if (XM <= Ym) {
-    for (int i = XM + 1; i <= Ym; ++i) {
-      if (i > Ym) break;
-      if (X < i && i <= Y) flag = true;
-    }
-  }
-
-  if (flag) cout << "No War" << endl;
-  else cout << "War" << endl;
-
-  // cin >> N;
-  // for (auto it = A.begin(); it < A.begin() + N; ++it) cin >> *it;
-
-  // for (auto it = A.begin(); it < A.begin() + N; ++it) if (*it % 2 == 0) ++num;
-
-  // cout << num << endl;
+  cout << num << endl;
   
   return 0;
 }
