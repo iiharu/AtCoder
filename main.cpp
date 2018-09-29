334d6121527c2ade72378ce62f2c85825cc56e98
iiharu <8288781+iiharu@users.noreply.github.com>
2018-09-29 23:04:42 +0900

    Map example

--- main.cpp
+++ main.cpp
@@ -18,16 +18,40 @@ signed main(void) {
   // num = numeric_limits<int>::max(); // min
   int N;
   // int M;
-  array<int, 100000> A;;
+  array<int, 100000> A;
   // string S;
   // string T;
+  map<int, int> m;
 
+  vector<pair<int, int>> v;
+  
   cin >> N;
-  for (auto it = A.begin(); it < A.begin() + N; ++it) cin >> *it;
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < i; ++j) {
+      m[i]++;
+    }
+  }
 
-  for (auto it = A.begin(); it < A.begin() + N; ++it) if (*it % 2 == 0) ++num;
+  for (auto it = m.begin(); it != m.end(); ++it) {
+    cout << (*it).first << ": " << (*it).second << endl;
+    v.push_back(make_pair((*it).second,(*it).first));
+  }
 
-  cout << num << endl;
+  sort(v.begin(), v.end());
+
+  cout << "Reverse" << endl;
+  
+  for (auto it = v.begin(); it != v.end(); ++it) {
+    cout << (*it).first << ": " << (*it).second << endl;
+  }
+  
+  
+  // cin >> N;
+  // for (auto it = A.begin(); it < A.begin() + N; ++it) cin >> *it;
+
+  // for (auto it = A.begin(); it < A.begin() + N; ++it) if (*it % 2 == 0) ++num;
+
+  // cout << num << endl;;
   
   return 0;
 }
