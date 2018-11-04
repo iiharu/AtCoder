992e2a5bb8cbedf656b20d7c68ce7ab3adc9f397
iiharu <8288781+iiharu@users.noreply.github.com>
2018-11-04 23:07:10 +0900

    [AtCoder ABC 113] A - Discount Fare

--- main.cpp
+++ main.cpp
@@ -8,12 +8,19 @@ signed main(void) {
   int num; // num
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
-  num = numeric_limits<int>::max(); // min
+  // num = numeric_limits<int>::max(); // min
   // int N;
   // int M;
   // array<int, 100000> A;
   // string S;
   // string T;
+
+  int X, Y;
+
+  cin >> X;
+  cin >> Y;
+
+  cout << X + Y / 2 << endl;
 
 
   return 0;
