8c17b8585b718c885cd0e27c5179bbeb8753f33d
iiharu <8288781+iiharu@users.noreply.github.com>
2018-11-04 23:03:23 +0900

    Update utils.cpp
    - Add gcd (gcd is implemented in C++17)

--- main.cpp
+++ main.cpp
@@ -4,41 +4,17 @@ using namespace std;
 
 #define int long long
 
-int gcd(int a, int b) {
-  if (b == 0) return a;
-  else if (a < b) return gcd(b, a);
-  else return gcd(b, a % b);
-}
-
-
 signed main(void) {
   int num; // num
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   num = numeric_limits<int>::max(); // min
-  int N;
+  // int N;
   // int M;
-  array<int, 100000> A;
+  // array<int, 100000> A;
   // string S;
   // string T;
-  int T;
 
-
-  cin >> N >> T;
-  for (int i = 0; i < N; ++i) {
-    int c, t;
-    cin >> c >> t;
-    if (t <= T) {
-      num = min(num, c);
-    }
-  }
-
-  if (num == numeric_limits<int>::max()) {
-    cout << "TLE" << endl;
-  } else {
-    cout << num << endl;
-    
-  }
 
   return 0;
 }
--- utils.cpp
+++ utils.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 // gcd
+int gcd(int a, int b) {
+  if (b == 0) return a;
+  else if (a < b) return gcd(b, a);
+  else return gcd(b, a % b);
+}
 // mode
 // median
 // max
