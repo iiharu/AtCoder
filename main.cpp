53fb2f7a46ddc8c49ed7fde9b58491176aceb7a7
iiharu <8288781+iiharu@users.noreply.github.com>
2018-11-05 19:17:49 +0900

    [AtCoder ABC 113] B - Palace

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #define int long long
 
+
 signed main(void) {
   int num; // num
   num = 0; // count, result
@@ -11,9 +12,28 @@ signed main(void) {
   // num = numeric_limits<int>::max(); // min
   int N;
   // int M;
-  array<int, 100000> A;
+  // array<int, 100000> A;;
   // string S;
   // string T;
+  int T, A;
+  array<double, 1000> H;
+
+  cin >> N;
+  cin >> T>>A;
+
+  for (int i = 0; i < N; ++i) {
+    int h;
+    cin >> h;
+    H[i] = abs(A - T + h * 0.006);
+  }
+
+  num = 0;
+  for (int i = 1; i < N; ++i) {
+    if (H[i] < H[num]) num = i;
+    
+  }
+
+  cout << num + 1 << endl;
 
   return 0;
 }
