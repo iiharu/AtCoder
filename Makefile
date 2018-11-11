f1094965e4e000fba8be61913760db4557ec154b
iiharu <8288781+iiharu@users.noreply.github.com>
2018-11-11 15:08:47 +0900

    Add astyle format option to Makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ CXX := g++
 CXXFLAGS := -std=c++14 -Wall -Wextra -Wc++14-compat
 LDFLAGS := -lc -lstdc++
 TARGET := main
+ASTYLE := astyle
 
 .PHONY: all clean
 
@@ -12,6 +13,9 @@ main: main.o
 
 %.o: %.cpp
 	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+astyle: $(TARGET).cpp
+	$(ASTYLE) $(TARGET).cpp
 
 clean:
 	$(RM) $(TARGET) $(wildcard *.d) $(wildcard *.o) $(wildcard *.out)
--- main.cpp
+++ main.cpp
@@ -4,18 +4,17 @@ using namespace std;
 
 // #define int long long
 
-signed main(void) {
+signed main(void)
+{
   int num; // num
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
   int N;
   int M;
-  
   array<int, 100000> P;
   array<int, 100000> Y;
   array<vector<int>, 100001> YD;
-
   cin >> N >> M;
 
   for (int i = 0; i < M; i++) {
@@ -23,14 +22,13 @@ signed main(void) {
     YD[P[i] - 1].push_back(Y[i]);
   }
 
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++)
     sort(YD[i].begin(), YD[i].end());
-  }
-  
+
   for (int i = 0; i < M; i++) {
     cout << setfill('0') << setw(6) << right << P[i]
          << setfill('0') << setw(6) << right << (lower_bound(YD[P[i] - 1].begin(), YD[P[i] - 1].end(), Y[i]) - YD[P[i] - 1].begin()) + 1 << endl;
   }
-  
+
   return 0;
 }
