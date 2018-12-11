caf4d5b3e9cb395d6da40ca0b1c26282e66f5316
iiharu <8288781+iiharu@users.noreply.github.com>
2018-12-11 15:01:08 +0900

    [AtCoder ABC 114] B - 754

--- main.cpp
+++ main.cpp
@@ -3,25 +3,20 @@ using namespace std;
 
 // #define int long long
 
-signed main(void)
-{
+signed main(void) {
   int num; // num
   // num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   num = numeric_limits<int>::max(); // min
   int n;
-  int k;
-  array<int, 100000> a;
+  string s;
 
-  cin>> n >> k;
-  for(auto it = a.begin(); it < a.begin() + n; ++it){
-    cin >> *it;
+  cin >> s;
+
+  for(auto it = s.begin();it+2!=s.end();++it){
+    int tmp = abs(100 * (*it - '0') + 10 * (*(it + 1) - '0') + (*(it + 2) - '0') - 753);
+    num = min(tmp, num);
   }
-
-  sort(a.begin(),a.begin() + n,greater<int>());
-
-  for(auto it = a.begin(); it + k - 1< a.begin() + n; ++it)
-    num = min(num, *it - *(it + k - 1));
 
   cout << num << endl;
   return 0;
