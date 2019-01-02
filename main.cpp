39af554a497167ddc1698ad4fbba27af5793b972
iiharu <8288781+iiharu@users.noreply.github.com>
2019-01-02 19:04:00 +0900

    [AtCoder Language Test] D - 複数形

--- main.cpp
+++ main.cpp
@@ -9,23 +9,10 @@ signed main(void) {
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
-  int n;
-  string c;
-  int M = 0;
-  int m = 100;
-  
-  cin >> n;
-  cin >> c;
-  
-  for (char i = '1'; i <= '4'; ++i) {
-    int tmp = 0;
-    for (auto it = c.begin(); it != c.end(); ++it) {
-      if (*it == i) tmp++;
-    }
-    M = max(M, tmp);
-    m = min(m, tmp);
-  }
 
-  cout << M << " " << m << endl;
+  string s;
+  cin >> s;
+  cout << s << 's' << endl;
+
   return 0;
 }
