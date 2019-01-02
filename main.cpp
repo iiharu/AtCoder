9e313cc33c1ebd0da11c6e2f837e6b0832994801
iiharu <8288781+iiharu@users.noreply.github.com>
2019-01-02 19:14:52 +0900

    [AtCoder Language Test] E - N重丸

--- main.cpp
+++ main.cpp
@@ -5,14 +5,26 @@ using namespace std;
 #define int long long
 
 signed main(void) {
-  int num; // num
+  // int num; // num
+  double num;
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
+  int n;
+  array<int, 1000> r;
 
-  string s;
-  cin >> s;
-  cout << s << 's' << endl;
+  cin >> n;
+  for(int i = 0; i < n; ++i) cin >> r[i];
+  sort(r.begin(), r.begin() + n);
+
+  for (int i = n - 1; 0 <= i; i-=2){
+    num += r[i] * r[i] * M_PI;
+  }
+  for (int i = n - 2; 0 <= i; i-=2) {
+    num -= r[i] * r[i] * M_PI;
+  }
+
+  cout << setprecision(10) << num << endl;
 
   return 0;
 }
