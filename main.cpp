51b9d76d2b7ec5ba162349fa7699c1d924f1007b
iiharu <8288781+iiharu@users.noreply.github.com>
2019-01-12 21:43:13 +0900

    update

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 // #include "boost/math/common_factor.hpp" // gcd, lcm
 using namespace std;
 
-#define int long long
+// #define int long long
 
 signed main(void) {
-  // int num; // num
-  double num;
+  int num; // num
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
