9cbe73a02081d057ca26bd5ead9275c0483f0906
iiharu <8288781+iiharu@users.noreply.github.com>
2019-01-21 14:42:08 +0900

    [AtCoder ABC116] B - Collatz Problem

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include "bits/stdc++.h"
 // #include "boost/math/common_factor.hpp" // gcd, lcm
 using namespace std;
@@ -10,8 +11,23 @@ signed main(void) {
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
   int n;
+  int s;
+  array<int, 1000000> a;
+  cin >> s;
 
-  cin >> n;
+  for (auto it = a.begin(); it < a.end(); ++it) {
+    ++num;
+    if (it == a.begin())
+      *it = s;
+    else
+      if (*(it - 1) % 2 == 0)
+        *it = *(it - 1) / 2;
+      else
+        *it = 3 * *(it - 1) + 1;
+    
+    if (find(a.begin(), it, *it) != it)
+      break;
+  }
 
   cout << num << endl;
   return 0;
