12d91d2a26f71c688683ec0bd7bd74ebb6317cd3
iiharu <8288781+iiharu@users.noreply.github.com>
2019-01-23 22:51:41 +0900

    CMakeLists.txt: remove -Wc++14-compat from compile option.

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,8 +6,8 @@
   (flycheck-clang-language-standard . "c++14")
   (flycheck-gcc-include-path "./include")
   (flycheck-clang-include-path "./include")
-  (flycheck-gcc-warnings "all" "extra" "c++14-compat")
-  (flycheck-clang-warnings "all" "extra" "c++14-compat")
+  (flycheck-gcc-warnings "all" "extra")
+  (flycheck-clang-warnings "all" "extra")
   ))
 
 
