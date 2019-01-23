6376fc3bdc34b10aa541102247b0909cd583c588
iiharu <8288781+iiharu@users.noreply.github.com>
2019-01-23 21:58:10 +0900

    .gitignore: add .dir-locals.el

--- .dir-locales.el
+++
@@ -1,6 +0,0 @@
-;; .dir-locales.el
-
-((c++-mode . ((company-clang-arguments . ("-std=c++14"))
-              (flycheck-gcc-language-standard . ("c++14"))
-              (flycheck-clang-language-standard . ("c++14"))))
- (nil . ()))
