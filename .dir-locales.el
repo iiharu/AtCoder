7a6ad7078bfde429651a9510afa63860460da969
iiharu <8288781+iiharu@users.noreply.github.com>
2019-01-23 17:11:37 +0900

    .dir-locales.el: update

--- .dir-locales.el
+++ .dir-locales.el
@@ -1,1 +1,6 @@
-((nil . ((company-clang-arguments . ("-std=c++14")))))
+;; .dir-locales.el
+
+((c++-mode . ((company-clang-arguments . ("-std=c++14"))
+              (flycheck-gcc-language-standard . ("c++14"))
+              (flycheck-clang-language-standard . ("c++14"))))
+ (nil . ()))
