8c71cea1f046ce673503f69a87a11a8f9c8aa2ab
iiharu <8288781+iiharu@users.noreply.github.com>
2019-01-23 22:40:43 +0900

    .dir-locals.el: add file

---
+++ .dir-locals.el
@@ -0,0 +1,15 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (flycheck-gcc-language-standard . "c++14")
+  (flycheck-clang-language-standard . "c++14")
+  (flycheck-gcc-include-path "./include")
+  (flycheck-clang-include-path "./include")
+  (flycheck-gcc-warnings "all" "extra" "c++14-compat")
+  (flycheck-clang-warnings "all" "extra" "c++14-compat")
+  ))
+
+
+
+
