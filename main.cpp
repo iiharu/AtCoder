70c08a8dc7942daed37d37430fd855f6de13f320
iiharu <8288781+iiharu@users.noreply.github.com>
2019-02-08 17:19:01 +0900

    bits/stdc++.h: update

--- main.cpp
+++ main.cpp
@@ -19,28 +19,9 @@ signed main(void) {
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
   int n; cin >> n;
-  int m; cin >> m;
-
-  vector<int> point(m);
-  for (auto it = point.begin(); it < point.end(); ++it) cin >> *it;
-
-  if (n >= m) {
-    num = 0;
-  } else {
-
-    sort(point.begin(), point.end());
-
-    vector<int> diff(m - 1);
-    for (int i = 0; i < m - 1; ++i) diff[i] = abs(point[i+1] - point[i]);
-
-    sort(diff.begin(), diff.end());
-
-    for (int i = 0; i < m - 1 - (n - 1); ++i) num += diff[i];
-  }
-  
-  // array<int, 1000000> a;
-  // for (auto it = a.begin(); it < a.begin() + n; ++it) cin >> *it;
-  // for (auto it = a.begin(); it < a.begin() + n; ++it) num += *it;
+  array<int, SIZE> a;
+  for (auto it = a.begin(); it < a.begin() + n; ++it) cin >> *it;
+  for (auto it = a.begin(); it < a.begin() + n; ++it) num += *it;
 
   cout << num << endl;
   return 0;
