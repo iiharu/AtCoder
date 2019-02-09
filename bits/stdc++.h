625d0ba9c814ed5b6f4ca72ffe61d388f61051ad
iiharu <8288781+iiharu@users.noreply.github.com>
2019-02-09 23:19:14 +0900

    bits/stdc++.h: update

--- bits/stdc++.h
+++ bits/stdc++.h
@@ -22,60 +22,60 @@
 // C++
 #include <algorithm>
 #include <bitset>
-#include <complex>
+/* #include <complex> */
 #include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
+/* #include <exception> */
+/* #include <fstream> */
+/* #include <functional> */
 #include <iomanip>
-#include <ios>
-#include <iosfwd>
+/* #include <ios> */
+/* #include <iosfwd> */
 #include <iostream>
-#include <istream>
+/* #include <istream> */
 #include <iterator>
 #include <limits>
 #include <list>
-#include <locale>
+/* #include <locale> */
 #include <map>
-#include <memory>
-#include <new>
+/* #include <memory> */
+/* #include <new> */
 #include <numeric>
-#include <ostream>
+/* #include <ostream> */
 #include <queue>
 #include <set>
-#include <sstream>
+/* #include <sstream> */
 #include <stack>
-#include <stdexcept>
-#include <streambuf>
+/* #include <stdexcept> */
+/* #include <streambuf> */
 #include <string>
-#include <typeinfo>
+/* #include <typeinfo> */
 #include <utility>
-#include <valarray>
+/* #include <valarray> */
 #include <vector>
 
-#if __cplusplus >= 201103L
-#include <array>
-#include <atomic>
-#include <chrono>
-#include <codecvt>
-#include <condition_variable>
-#include <forward_list>
-#include <future>
-#include <initializer_list>
-#include <mutex>
-#include <random>
-#include <ratio>
-#include <regex>
-#include <scoped_allocator>
-#include <system_error>
-#include <thread>
-#include <tuple>
-#include <typeindex>
-#include <type_traits>
-#include <unordered_map>
-#include <unordered_set>
-#endif
+/* #if __cplusplus >= 201103L */
+/* #include <array> */
+/* #include <atomic> */
+/* #include <chrono> */
+/* #include <codecvt> */
+/* #include <condition_variable> */
+/* #include <forward_list> */
+/* #include <future> */
+/* #include <initializer_list> */
+/* #include <mutex> */
+/* #include <random> */
+/* #include <ratio> */
+/* #include <regex> */
+/* #include <scoped_allocator> */
+/* #include <system_error> */
+/* #include <thread> */
+/* #include <tuple> */
+/* #include <typeindex> */
+/* #include <type_traits> */
+/* #include <unordered_map> */
+/* #include <unordered_set> */
+/* #endif */
 
-#if __cplusplus >= 201402L
-#include <shared_mutex>
-#endif
+/* #if __cplusplus >= 201402L */
+/* #include <shared_mutex> */
+/* #endif */
