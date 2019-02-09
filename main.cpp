b7d41b793566139222d8bc0fc2923e0da5a17d18
iiharu <8288781+iiharu@users.noreply.github.com>
2019-02-09 21:29:12 +0900

    [AtCoder みんなのプロコン 2019] C - When I hit my pocket...

--- main.cpp
+++ main.cpp
@@ -18,10 +18,45 @@ signed main(void) {
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
-  int n; cin >> n;
-  array<int, SIZE> a;
-  for (auto it = a.begin(); it < a.begin() + n; ++it) cin >> *it;
-  for (auto it = a.begin(); it < a.begin() + n; ++it) num += *it;
+  // int n; cin >> n;
+
+  int k; cin >> k;
+  int a; cin >> a;
+  int b; cin >> b;
+
+  num = 1;
+  int yen = 0;
+  
+  // if (a < b) {
+  if (b - a > 2) {
+    
+    for (int i = 0; i < k; ++i) {
+
+      if (yen > 0) {
+        --yen;
+        num += b;
+      } else { // yen == 0 => true
+        if (num < a) {
+          ++num;
+        } else {
+          if (i == k - 1) {
+            ++num;
+          } else {
+            ++yen;
+            num -= a;
+          }
+        }
+      }
+      // cout << "num: " << num << " yen: " << yen << endl;
+    }
+    
+  } else {
+    num += k;
+  }
+  
+  // array<int, SIZE> a;
+  // for (auto it = a.begin(); it < a.begin() + n; ++it) cin >> *it;
+  // for (auto it = a.begin(); it < a.begin() + n; ++it) num += *it;
 
   cout << num << endl;
   return 0;
