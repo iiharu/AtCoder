443410dca92cfbcea9965742e44134292d786d14
iiharu <8288781+iiharu@users.noreply.github.com>
2019-02-20 14:11:27 +0900

    [AtCoder ABC 116] C - Grand Garden

--- main.cpp
+++ main.cpp
@@ -15,10 +15,6 @@
 #include <utility>
 #include <vector>
 
-// #define rep(i, n) for(int i = 0; i < (int)(n); i++)
-// #define all(a) (a).begin(),(a).end()
-// #define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
-
 template <typename T> T clamp(T val, T min, T max) { return val < min ? min : (max < val ? max : val); }
 template <typename T> T gcd(T a, T b) { return b == 0 ? a : gcd(b, a % b); }
 template <typename T> T lcm(T a, T b) { return a * b / gcd(a, b); }
@@ -37,14 +33,39 @@ signed main(void) {
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
 
-  cout << gcd(1071, 1029) << endl;
-  cout << gcd(1029, 1071) << endl;
-  cout << lcm(30, 42) << endl;
-  
-  // array<int, SIZE> a;
-  // for (auto it = a.begin(); it < a.begin() + n; ++it) cin >> *it;
-  // for (auto it = a.begin(); it < a.begin() + n; ++it) num += *it;
+  int n; cin >> n;
+  vector<int> h(n);
+  for (auto it = h.begin(); it < h.end(); ++it) cin >> *it;
 
+  for (int i = 0; i <= 100; ++i) {
+
+    bool is_watering = false;
+    for (auto it = h.begin(); it < h.end(); ++it){
+      if (is_watering) {
+        if (*it <= i || it == h.end() - 1) {
+          is_watering = false;
+          ++num;
+        }
+      } else {
+        if (*it > i) {
+          if (it == h.end() - 1) {
+            ++num;
+          } else {
+            is_watering = true;
+          }
+        }
+      }
+    }
+    
+  }
+  
+  // int num = 0;
+  // int c; cin >> n;
+  // vector<int> v(n);
+  // for (auto it = v.begin(); it < v.end(); ++it) cin >> *it;
+  // for (auto it = v.begin(); it < v.end(); ++it) num += *it;
+  // cout << num << endl;
+  
   cout << num << endl;
   return 0;
 }
