423fed7172d1f65ccfc22684bbc5be1d5f30ccc3
iiharu <8288781+iiharu@users.noreply.github.com>
2019-02-20 10:54:49 +0900

    remove include/boost

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 
 #include "bits/stdc++.h"
-#include <boost/math/common_factor_rt.hpp>
 // #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 // #define all(a) (a).begin(),(a).end()
 // #define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
 // int gcd(int a, int b){return b ? gcd(b, a%b) : a;}
+
+template <typename T> T clamp(T val, T min, T max) { return val < min ? min : (max < val ? max : val); }
+template <typename T> T gcd(T a, T b) { return b == 0 ? a : gcd(b, a % b); }
+template <typename T> T lcm(T a, T b) { return a * b / gcd(a, b); }
 
 #define int long long
 #define SIZE 100000
@@ -19,23 +22,10 @@ signed main(void) {
   num = 0; // count, result
   // num = numeric_limits<int>::min(); // max
   // num = numeric_limits<int>::max(); // min
-  int n; cin >> n;
 
-  vector<int> a(n);
-  for (auto it = a.begin(); it < a.end(); ++it) cin >> *it;
-
-  while (a.size() != 1) {
-    sort(a.begin(), a.end());
-    auto it = lower_bound(a.begin(), a.end(), 1);
-    a.erase(a.begin(), it);
-    for (auto it = a.begin() + 1; it < a.end(); ++it) {
-      *it = *it % *(a.begin());
-    }
-    // ostream_iterator<int> oit(cout, " ");
-    // copy(a.begin(), a.end(), oit); cout << endl;
-  }
-
-  num = a.front();
+  cout << gcd(1071, 1029) << endl;
+  cout << gcd(1029, 1071) << endl;
+  cout << lcm(30, 42) << endl;
   
   // array<int, SIZE> a;
   // for (auto it = a.begin(); it < a.begin() + n; ++it) cin >> *it;
