a63fba4ec681b1f8e2e6f7ced25dfa99f8a8be4d
iiharu <8288781+iiharu@users.noreply.github.com>
2019-02-28 23:26:38 +0900

    main.cpp: add max_index, min_index

--- main.cpp
+++ main.cpp
@@ -24,11 +24,10 @@
 template <typename T> T gcd(T a, T b) { return b == 0 ? a : gcd(b, a % b); }
 template <typename T> T lcm(T a, T b) { return a * b / gcd(a, b); }
 template <typename T> T clamp(T val, T min, T max) { return val < min ? min : (max < val ? max : val); }
+template <typename T> size_t max_index(T begin, T end) { return distance(begin, max_element(begin, end)); }
+template <typename T> size_t min_index(T begin, T end) { return distance(begin, min_element(begin, end)); }
 
-#define int long long
-#define all(a) (a).begin(),(a).end()
-#define rep(i,n) for (int i=0;i<(n);++i)
-#define forit(beginIt, endIt) for(auto it=(beginIt);it!=(endIt);++it)
+// #define int long long
 
 using namespace std;
 
@@ -37,8 +36,7 @@ signed main(void) {
   ios::sync_with_stdio(false);
 
   int num = 0;
-  int n;
-  cin >> n;
+  int n; cin >> n;
   vector<int> v(n);
   forit (v.begin(), v.end()) cin >> *it;
   for (auto it = v.begin(); it < v.end(); ++it) num += *it;
