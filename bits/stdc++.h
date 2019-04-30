d90bbfed51769d9ce659663bc9eef54cd1e529e4
iiharu <8288781+iiharu@users.noreply.github.com>
2019-04-30 21:37:13 +0900

    bits/stdc++.h: sort

--- bits/stdc++.h
+++ bits/stdc++.h
@@ -1,22 +1,18 @@
 
-#include <algorithm>
-#include <bitset>
 #include <climits>
 #include <cmath>
-#include <cstddef>
 #include <cstdlib>
+
+#include <algorithm>
+#include <bitset>
 #include <deque>
 #include <functional>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <limits>
-#include <list>
 #include <map>
 #include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
 #include <string>
 #include <utility>
 #include <vector>
