1bc0503bfbe68594b6cd767339765956c4a996ef
iiharu <iiharu@alumni.tus.ac.jp>
2019-06-01 23:47:14 +0900

    main.cpp: update

--- main.cpp
+++ main.cpp
@@ -1,36 +1,27 @@
 
-#include "bits/stdc++.h"
-
-#define int long long
-
+#include <bits/stdc++.h>
 using namespace std;
 
+// #define int long long
+struct Fast { Fast(){ std::cin.tie(0); std::cout.tie(0); ios::sync_with_stdio(false); }} fast;
+
+#define ALL(v) begin(v), end(v)
+#define RALL(v) rbegin(v), rend(v)
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+
+#define debug(x) cerr << #x << ": " << x << endl;
+
+const int MOD = 1e9 + 7;
+
 signed main(void) {
-
-  int ans = 0;
-
+  
   int n; cin >> n;
   vector<int> a(n);
-  for (int i = 0; i < n; ++i) cin >> a[i];
-
-  vector<int> b(n);
-  for (int i = 0; i < n; ++i) {
-    if (i == 0) b[i] = a[i];
-    else b[i] = b[i - 1] + a[i];
-  }
-  vector<int> c(n);
-  for (int i = n - 1; i >= 0; --i) {
-    if (i == n - 1) c[i] = a[i];
-    else c[i] = c[i + 1] + a[i];
-  }
-
-  for (int i = 0; i < n - 1; ++i) {
-    int tmp = abs(b[i] - c[i + 1]);
-    if (i == 0) ans = tmp;
-    else ans = min(ans, tmp);
-  }
+  for (auto it = a.begin(); it != a.end(); ++it) cin >> *it;
   
-  cout << ans << endl;
-  
+  cout << accumulate(a.cbegin(), a.cend(), 0) << endl;
+
   return 0;
 }
