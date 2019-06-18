f913f62f88e2bd2f65cb4cc23027f6afa99a71ca
iiharu <iiharu@alumni.tus.ac.jp>
2019-06-18 18:34:33 +0900

    [AtCoder ABC130] A - Rounding

---
+++ a/main.cpp
@@ -0,0 +1,36 @@
+
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <bitset>
+#include <iostream>
+#include <deque>
+#include <functional>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <vector>
+
+using namespace std;
+
+#define int long long
+
+#define ALL(v) begin(v), end(v)
+#define RALL(v) rbegin(v), rend(v)
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+
+#define debug(x) cerr << #x << ": " << x << endl;
+
+const int MOD = 1e9 + 7;
+
+signed main(void)
+{
+  int x; cin >> x;
+  int a; cin >> a;
+  cout << (x < a ? 0 : 10) << endl;
+}
