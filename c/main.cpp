aa1e499a7f2bc13902c49ce12ea927f1567e5e1a
iiharu <iiharu@alumni.tus.ac.jp>
2019-07-01 15:19:19 +0900

    [AtCoder ABC132] C - Divide the Problems

--- c/main.cpp
+++ c/main.cpp
@@ -30,7 +30,20 @@ const int MOD = 1e9 + 7;
 
 signed main(void)
 {
-  int W, H; cin >> W >> H;
-  int x, y; cin >> x >> y;
-  cout << W * H / 2. << " " << (2 * x == W && 2 * y == H ? 1 : 0) << endl;
+  int num;
+  int n; cin >> n;
+  vector<int> d(n);
+  for (int i = 0; i < n; ++i) cin >> d[i];
+  sort(d.begin(), d.end());
+
+  num = 0;
+  for (int i = d.front(); i <= d.back(); ++i) {
+    int dist = distance(d.cbegin(), lower_bound(d.cbegin(), d.cend(), i));
+    if (n / 2 == dist) {
+      ++num;
+    } else if (n / 2 < dist) break;
+
+  }
+
+  cout << num << endl;
 }
