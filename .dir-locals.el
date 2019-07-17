846a1fe52ed36fed022f35051be0623d059d4c5a
iiharu <iiharu@alumni.tus.ac.jp>
2019-07-17 22:27:37 +0900

    Updat

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 	
 ((c++-mode
+  (company-clang-arguments . ("-std=c++14"))
 	(flycheck-gcc-language-standard . "c++14")
 	(flycheck-clang-language-standard . "c++14")
 	(flycheck-gcc-warnings "all" "extra")
