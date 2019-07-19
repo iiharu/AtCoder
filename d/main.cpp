e1a6815523dffdd32357ae59802161086b9508d7
iiharu <iiharu@alumni.tus.ac.jp>
2019-07-19 12:39:08 +0900

    [AtCoder ABC133] D - Rain Flows into Dams

--- d/main.cpp
+++ d/main.cpp
@@ -32,6 +32,25 @@ signed main(void)
 {
   int n; cin >> n;
   vector<int> a(n);
-  for (int i = 0; i < n; ++i) cin >> a[i];
+  vector<int> x(n);
+  int sum = 0;
+  x[0] = sum;
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+    sum += a[i];
+    if (i % 2 == 1) {
+      x[0] -= 2 * a[i];
+    }
+  }
+
+  x[0] += sum;
+
+  for (int i = 0; i < n; ++i) {
+    if (i != 0) {
+      x[i] = 2 * a[i - 1] - x[i - 1];
+    }
+    cout << x[i];
+    cout << " ";
+  }
 
 }
