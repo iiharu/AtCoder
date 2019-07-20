ff4661907830cae5fec815e4627ed64838331b9e
iiharu <iiharu@alumni.tus.ac.jp>
2019-07-20 18:45:48 +0900

    Remove ignore file

--- dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-	
-((c++-mode
-  (company-clang-arguments . ("-std=c++14"))
-  (flycheck-gcc-language-standard "c++14")
-  (flycheck-clang-language-standard "c++14")
-  (flycheck-gcc-warnings "all" "extra")
-  (flycheck-clang-warnings "all" "extra")))
