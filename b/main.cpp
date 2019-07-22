82e67aa9863955f66cde79bf2321dd3797808ad3
iiharu <iiharu@alumni.tus.ac.jp>
2019-07-22 20:22:43 +0900

    [AtCoder ABC134] B - Golden Apple

--- b/main.cpp
+++ b/main.cpp
@@ -27,38 +27,18 @@ using namespace std;
 
 const int MOD = 1e9 + 7;
 
-bool isSquare(int n) {
-  for (int i = 0; i <= sqrt(n); ++i) {
-    if (i * i == n) return true;
-  }
-  return false;
-}
-
 signed main(void)
 {
+  int num;
+  int n;
+  int d;
+  cin >> n >> d;
 
-  int num = 0;
-
-  int n; cin >> n;
-  int d; cin >> d;
-
-  vector<vector<int>> x(n);
-  for (int i = 0; i < n; ++i) {
-    x[i] = vector<int>(d);
-    for (int j = 0; j < d; ++j) {
-      cin >> x[i][j];
+  for (int i = 0; i <= n; ++i) {
+    if (n <= (2 * d + 1) * i) {
+      cout << i << endl;
+      break;
     }
   }
 
-  for (int i = 0; i < n; ++i) {
-    for (int j = i + 1; j < n; ++j) {
-      int dist = 0;
-      for (int k = 0; k < d; ++k) {
-        dist += (x[i][k] - x[j][k]) * (x[i][k] - x[j][k]);
-      }
-      if (isSquare(dist)) ++num;
-    }
-  }
-
-  cout << num << endl;
 }
