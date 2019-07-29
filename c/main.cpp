9b0e3e4d247fe9f7687ba6197af2f5537838f3fd
iiharu <iiharu@alumni.tus.ac.jp>
2019-07-29 18:41:58 +0900

    [AtCoder ABC135] C - City Savers

--- c/main.cpp
+++ c/main.cpp
@@ -33,29 +33,26 @@ template <typename T> T lcm(T m, T n) { return n * m / gcd(n, m); }
 signed main(void)
 {
   int num;
-
   int n; cin >> n;
-  vector<int> a(n);
-  for (int i = 0; i < n; ++i) cin >> a[i];
+  vector<int> a(n + 1);
+  for (int i = 0; i < n + 1; ++i) cin >> a[i];
 
   vector<int> b(n);
-  vector<int> c(n);
-  b[0] = a[0];
-  c[n - 1] = a[n - 1];
-  
-  for (int i = 1; i < n; ++i) {
-    b[i] = max(a[i], b[i - 1]);
-    c[n - 1 - i] = max(a[n - 1 - i], c[n - i]);
-  }
+  for (int i = 0; i < n; ++i) cin >> b[i];
+
+  num = 0;
 
   for (int i = 0; i < n; ++i) {
-    if (i == 0) {
-      cout << c[i + 1] << endl;
-    } else if (i == n - 1) {
-      cout << b[i - 1] << endl;
-    } else {
-      cout << max(b[i - 1], c[i + 1]) << endl;
+    int m;
+
+    for (int j = 0; j <= 1; ++j) {
+      m = min(b[i], a[i + j]);
+      a[i + j] -= m;
+      b[i] -= m;
+      num += m;
     }
   }
+  
+  cout << num << endl;
   
 }
