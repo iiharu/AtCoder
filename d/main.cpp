e7eb6dbf77579f9c97e40c4402d9d8676aa67f7b
iiharu <iiharu@alumni.tus.ac.jp>
2019-08-28 15:41:55 +0900

    [AtCoder ARC001] B - リモコン

--- d/main.cpp
+++ d/main.cpp
@@ -28,60 +28,35 @@ using namespace std;
 
 const int MOD = 1e9 + 7;
 
-// void add(int n, vector<pair<int, vector<int>>> &t) {
-//   for (int i = 0; i < t[n].second.size(); ++i) {
-//     int m = t[n].second[i];
-//     t[m].first += t[n].first;
-//     add(m, t);
-//   }
-// }
-
-void add(int n, vector<tuple<int, int, vector<int>>> &t) {
-  if (get<1>(t[n]) == 1) return;
-  get<1>(t[n]) = 1;
-  for (int i = 0; i < get<2>(t[n]).size(); ++i) {
-    int m = get<2>(t[n])[i];
-    if (get<1>(t[m]) == 1) continue;
-    get<0>(t[m]) += get<0>(t[n]);
-    add(m, t);
-  }
-}
-
 signed main(void)
 {
-  int n; cin >> n;
-  int q; cin >> q;
+  int num;
+  int a, b; cin >> a >> b;
+  queue<pair<int, int>> q;
+  q.push(make_pair(0, a));
 
-  // vector<pair<int, vector<int>>> t(n);
-  vector<tuple<int, int, vector<int>>> t(n);
-  for (int i = 0; i < n; ++i) {
-    // t[i] = make_pair(0, vector<int>());
-    t[i] = make_tuple(0, -1 ,vector<int>());
-  }
-  for (int i = 0; i < n - 1; ++i) {
-    int a, b; cin >> a >> b;
-    get<2>(t[a - 1]).push_back(b - 1);
-    get<2>(t[b - 1]).push_back(a - 1);
-    // t[a - 1].second.push_back(b - 1);
-    // t[b - 1].second.push_back(a - 1);
-  }
-  // cout << "input" << endl;
+  num = -1;
+  while(true) {
 
-  for (int i = 0; i < q; ++i) {
-    int p, x; cin >> p >> x;
-    get<0>(t[p - 1]) += x;
-    // t[p - 1].first += x;
-  }
-
-  add(0, t);
-
-  for (int i = 0; i < n; ++i) {
-    cout << get<0>(t[i]);
-    // cout << t[i].first;
-    if (i != n - 1) cout << " ";
-    else cout << endl;
+    auto e = q.front();
+    q.pop();
+    int m = e.first;
+    int c = e.second;
+    if (num != -1 && m > num) {
+      break;
+    }
+    if (c == b) {
+      num = m;
+    }
+    q.push(make_pair(m + 1, c - 1));
+    q.push(make_pair(m + 1, c + 1));
+    q.push(make_pair(m + 1, c - 5));
+    q.push(make_pair(m + 1, c + 5));
+    q.push(make_pair(m + 1, c - 10));
+    q.push(make_pair(m + 1, c + 10));
   }
 
+  cout << num << endl;
   
 }
 
