117cef56a9042b3ee43d32c76e135ea3155e31a1
iiharu <iiharu@alumni.tus.ac.jp>
2019-09-20 05:36:10 +0000

    [AtCoder ABC139] D - ModSum

--- d/main.cpp
+++ d/main.cpp
@@ -31,32 +31,10 @@ const int MOD = 1e9 + 7;
 signed main(void)
 {
   int num;
-  int a, b; cin >> a >> b;
-  queue<pair<int, int>> q;
-  q.push(make_pair(0, a));
+  int n;
+  cin >> n;
 
-  num = -1;
-  while(true) {
-
-    auto e = q.front();
-    q.pop();
-    int m = e.first;
-    int c = e.second;
-    if (num != -1 && m > num) {
-      break;
-    }
-    if (c == b) {
-      num = m;
-    }
-    q.push(make_pair(m + 1, c - 1));
-    q.push(make_pair(m + 1, c + 1));
-    q.push(make_pair(m + 1, c - 5));
-    q.push(make_pair(m + 1, c + 5));
-    q.push(make_pair(m + 1, c - 10));
-    q.push(make_pair(m + 1, c + 10));
-  }
+  num = (n - 1) * n / 2;
 
   cout << num << endl;
-  
 }
-
