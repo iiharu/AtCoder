7679d35932fcb5a2f9a055973a689abe73f93787
iiharu <iiharu@alumni.tus.ac.jp>
2019-09-30 09:54:50 +0000

    [AtCoder ABC142] B - Roller Coaster

--- b/main.cpp
+++ b/main.cpp
@@ -30,19 +30,19 @@ const int MOD = 1e9 + 7;
 
 signed main(void)
 {
-  string s; cin >> s;
-  vector<bool> v(s.length(), false);
-
-  for (int i = 0; i < s.length(); ++i) {
-        if ((i % 2 == 0 && s[i] == 'R')
-            || (i % 2 == 1 && s[i] == 'L')
-            || s[i] == 'U'
-            || s[i] == 'D')
-          v[i] = true;
+  int num;
+  int n;
+  cin >> n;
+  int k;
+  cin >> k;
+  num = 0;
+  for (int i = 0; i < n; ++i)
+  {
+    int h;
+    cin >> h;
+    if (h >= k)
+      ++num;
   }
 
-  bool flag = accumulate(v.begin(), v.end(),true, [](bool acc, bool i) { return acc & i;});
-  if (flag) cout << "Yes";
-  else cout << "No";
-  cout << endl;
+  cout << num << endl;
 }
