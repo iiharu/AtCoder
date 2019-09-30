ad52c3e205f314bc4c29c8ad41880605dedb7003
iiharu <iiharu@alumni.tus.ac.jp>
2019-09-30 10:49:01 +0000

    [AtCoder ABC 142] D - Disjoint Set of Common Divisors

--- d/main.cpp
+++ d/main.cpp
@@ -28,13 +28,60 @@ using namespace std;
 
 const int MOD = 1e9 + 7;
 
+template <typename T>
+T gcd(T m, T n) { return n == 0 ? m : gcd(n, m % n); }
+template <typename T>
+T lcm(T m, T n) { return n * m / gcd(n, m); }
+
+void primeFactorize(int n, vector<int> &v)
+{
+  v = vector<int>(n + 1, 0);
+  int a = 2;
+  while (n >= a * a)
+  {
+    if (n % a == 0)
+    {
+      ++v[a];
+      n /= a;
+    }
+    else
+    {
+      a++;
+    }
+  }
+  v[n]++;
+}
+
 signed main(void)
 {
   int num;
-  int n;
-  cin >> n;
 
-  num = (n - 1) * n / 2;
+  int a, b;
+  cin >> a >> b;
 
+  num = 1;
+  int n = min(sqrt(a), sqrt(b));
+  for (int i = 2; i <= n; ++i)
+  {
+    int ca = 0;
+    int cb = 0;
+    while (a % i == 0)
+    {
+      a /= i;
+      ++ca;
+    }
+    while (b % i == 0)
+    {
+      b /= i;
+      ++cb;
+    }
+    if (ca > 0 && cb > 0)
+    {
+      ++num;
+    }
+  }
+
+  if (a == b && a > n)
+    ++num;
   cout << num << endl;
 }
