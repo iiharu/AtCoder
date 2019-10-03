6a93826359e118e53a28eedff8731ee4bdbfffa6
iiharu <iiharu@alumni.tus.ac.jp>
2019-10-03 07:27:52 +0000

    [AtCoder ABC141] D - Powerful Discount Tickets

--- d/main.cpp
+++ d/main.cpp
@@ -33,55 +33,39 @@ T gcd(T m, T n) { return n == 0 ? m : gcd(n, m % n); }
 template <typename T>
 T lcm(T m, T n) { return n * m / gcd(n, m); }
 
-void primeFactorize(int n, vector<int> &v)
-{
-  v = vector<int>(n + 1, 0);
-  int a = 2;
-  while (n >= a * a)
-  {
-    if (n % a == 0)
-    {
-      ++v[a];
-      n /= a;
-    }
-    else
-    {
-      a++;
-    }
-  }
-  v[n]++;
-}
-
 signed main(void)
 {
   int num;
+  int n, m;
+  cin >> n >> m;
 
-  int a, b;
-  cin >> a >> b;
-
-  num = 1;
-  int n = min(sqrt(a), sqrt(b));
-  for (int i = 2; i <= n; ++i)
+  priority_queue<int> q;
+  for (int i = 0; i < n; ++i)
   {
-    int ca = 0;
-    int cb = 0;
-    while (a % i == 0)
-    {
-      a /= i;
-      ++ca;
-    }
-    while (b % i == 0)
-    {
-      b /= i;
-      ++cb;
-    }
-    if (ca > 0 && cb > 0)
-    {
-      ++num;
-    }
+    int a;
+    cin >> a;
+    q.push(a);
   }
 
-  if (a == b && a > n)
-    ++num;
+  while (m > 0)
+  {
+    int a = q.top();
+    q.pop();
+    a /= 2;
+    q.push(a);
+    --m;
+  }
+
+  // while (!q.empty())
+  // {
+  //   cout << q.top() << endl;
+  //   q.pop();
+  // }
+  num = 0;
+  while (!q.empty())
+  {
+    num += q.top();
+    q.pop();
+  }
   cout << num << endl;
 }
