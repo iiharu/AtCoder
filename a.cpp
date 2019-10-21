07f6a79534a8c839bace656fcbbf314f15e17f0a
iiharu <iiharu@alumni.tus.ac.jp>
2019-10-21 10:10:57 +0000

    add bits/stdc++.h

--- a.cpp
+++ a.cpp
@@ -1,18 +1,5 @@
 
-#include <cmath>
-#include <cstdlib>
-#include <algorithm>
-#include <bitset>
-#include <iostream>
-#include <deque>
-#include <functional>
-#include <iterator>
-#include <limits>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <string>
-#include <vector>
+#include "bits/stdc++.h"
 
 using namespace std;
 
---
+++ bits/stdc++.h
@@ -0,0 +1,15 @@
+
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <bitset>
+#include <iostream>
+#include <deque>
+#include <functional>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <string>
+#include <vector>
