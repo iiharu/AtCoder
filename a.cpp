aa55f4e2110e15b571a875967cdeb473afa0172a
iiharu <iiharu@alumni.tus.ac.jp>
2019-11-01 08:28:30 +0000

    [AtCoder ARC044] A - 素数判定

--- a.cpp
+++ a.cpp
@@ -19,11 +19,48 @@ template <typename T> T gcd(T m, T n) { return n == 0 ? m : gcd(n, m % n); }
 template <typename T> T lcm(T m, T n) { return n * m / gcd(n, m); }
 
 signed main(void) {
-  int a, b;
-  cin >> a >> b;
+  bool flag = true;
+  int n;
+  cin >> n;
 
-  if (1 <= a && a <= 9 && 1 <= b && b <= 9)
-    cout << a * b << endl;
+  if (n != 1) {
+
+    for (int i = 2; i <= sqrt(n); ++i) {
+      if (n % i == 0) {
+        flag = false;
+        break;
+      }
+    }
+
+    if (!flag) {
+      string s = to_string(n);
+
+      switch (s[s.length() - 1]) {
+      case '0':
+      case '2':
+      case '4':
+      case '5':
+      case '6':
+      case '8':
+        flag = false;
+        break;
+      default:
+        flag = true;
+        break;
+      }
+
+      if (flag) {
+        if (n % 3 == 0) {
+          flag = false;
+        }
+      }
+    }
+  } else {
+    flag = false;
+  }
+
+  if (flag)
+    cout << "Prime" << endl;
   else
-    cout << -1 << endl;
+    cout << "Not Prime" << endl;
 }
