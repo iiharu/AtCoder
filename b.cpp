ee3c237867a9433945813231de7bbb87fe4dc470
iiharu <iiharu@alumni.tus.ac.jp>
2019-11-06 05:53:37 +0000

    [AtCoder ARC007] B - 迷子のCDケース

--- b.cpp
+++ b.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-#define int long long
+// #define int long long
 
 #define ALL(v) begin(v), end(v)
 #define RALL(v) rbegin(v), rend(v)
@@ -19,19 +19,23 @@ template <typename T> T lcm(T m, T n) { return n * m / gcd(n, m); }
 const int MOD = 1e9 + 7;
 
 signed main(void) {
-  bool flag = true;
-  int n;
-  cin >> n;
+  int n, m;
+  cin >> n >> m;
 
-  for (int a = 9; a >= 1; --a) {
-    if (n % a == 0) {
-      n /= a;
-      break;
-    }
+  vector<int> music(m);
+  for (int i = 0; i < m; ++i)
+    cin >> music[i];
+
+  vector<int> cases(n + 1);
+  for (int i = 0; i < n + 1; ++i)
+    cases[i] = i;
+  for (int i = 0; i < m; ++i) {
+    auto it = find(cases.begin(), cases.end(), music[i]);
+    if (it != cases.end())
+      swap(cases[0], *(find(cases.begin(), cases.end(), music[i])));
   }
 
-  if (1 <= n && n <= 9)
-    cout << "Yes" << endl;
-  else
-    cout << "No" << endl;
+  for (auto it = cases.cbegin() + 1; it != cases.cend(); ++it) {
+    cout << *it << endl;
+  }
 }
