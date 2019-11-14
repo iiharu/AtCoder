52761910154edcacbdda969b009bdce31c0975c7
iiharu <iiharu@alumni.tus.ac.jp>
2019-11-14 08:32:04 +0000

    [AtCoder ABC064] C - Colorful Leaderboard

--- c.cpp
+++ c.cpp
@@ -13,10 +13,32 @@ template <typename T> T modpow(T a, T e, T m) { return (e == (T)0 ? 1 : ((a % m)
 constexpr int MOD = 1e9 + 7;
 
 signed main(void) {
+  int num;
   int n;
   cin >> n;
-  vector<int> a(n);
-  for (auto &e : a) cin >> e;
 
-  cout << accumulate(a.cbegin(), a.cend(), 0) << endl;
+  map<int, int> mem;
+  for (int i = 0; i < n; ++i) {
+    int a; cin >> a;
+    if (a < 3200)
+      mem[a / 400]++;
+    else
+      mem[8]++;
+  }
+
+  num = 0;
+  for (int j = 0; j < 8; ++j) {
+    if (mem[j] > 0) ++num;
+  }
+
+  int m = num;
+  int M = num;
+  if (mem[8] > 0) {
+    if (m == 0) ++m;
+    M += mem[8];
+    num = min(M, (int)8);
+  }
+
+  cout << m << " " << M << endl;
+
 }
