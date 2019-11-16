ef6235df46ea2c6584b2fd805b3d2bd36b4851a6
iiharu <iiharu@alumni.tus.ac.jp>
2019-11-16 13:57:05 +0000

    [AtCoder ABC145] D - Knight

--- d.cpp
+++ d.cpp
@@ -12,11 +12,53 @@ template <typename T> T modpow(T a, T e, T m) { return (e == (T)0 ? 1 : ((a % m)
 
 constexpr int MOD = 1e9 + 7;
 
-signed main(void) {
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  for (auto &e : a) cin >> e;
+const int MAX = 999999;
+long long fac[MAX], finv[MAX], inv[MAX];
 
-  cout << accumulate(a.cbegin(), a.cend(), 0) << endl;
+// テーブルを作る前処理
+void COMinit() {
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for (int i = 2; i < MAX; i++) {
+    fac[i] = fac[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+}
+
+// 二項係数計算
+long long COM(int n, int k) {
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int X;
+int Y;
+
+signed main(void) {
+  int num;
+  cin >> X >> Y;
+
+  COMinit();
+
+  int d;
+  for (d = 0;; ++d) {
+    if (X + d == 2 * (Y - d)) { break; }
+
+    if (Y - d <= 0) {
+      d = -1;
+      break;
+    }
+  }
+
+  if (d == -1) {
+    num = 0;
+  } else {
+
+    num = COM(Y - d, d);
+  }
+
+  cout << num << endl;
 }
