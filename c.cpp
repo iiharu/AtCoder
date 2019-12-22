d4c4efbd46ba984cfa332bbfae84158d00737982
iiharu <iiharu@alumni.tus.ac.jp>
2019-12-22 13:43:02 +0000

    [AtCoder ABC148] C - Snack

--- c.cpp
+++ c.cpp
@@ -13,28 +13,7 @@ template <typename T> T modpow(T a, T e, T m) { return (e == (T)0 ? 1 : ((a % m)
 constexpr int MOD = 1e9 + 7;
 
 signed main(void) {
-  int n;
-  cin >> n;
-
-  vector<pair<int, int>> p(n);
-  for (int i = 0; i < n; ++i) {
-    int x, y;
-    cin >> x >> y;
-    p[i] = make_pair(x, y);
-  }
-
-  sort(p.begin(), p.end());
-
-  double dist = 0;
-  do {
-    for (int i = 1; i < n; ++i) {
-      double d = (p[i - 1].first - p[i].first) * (p[i - 1].first - p[i].first) +
-                 (p[i - 1].second - p[i].second) * (p[i - 1].second - p[i].second);
-      dist += sqrt(d);
-    }
-  } while (next_permutation(p.begin(), p.end()));
-
-  dist /= (double)(fact(n));
-
-  cout << setprecision(10) << dist << endl;
+  int a, b;
+  cin >> a >> b;
+  cout << lcm(a, b) << endl;
 }
