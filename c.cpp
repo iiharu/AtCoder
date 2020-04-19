6f5e5ef8ddf33da3383f2c6a4890c650bf48f4ca
iiharu <iiharu@alumni.tus.ac.jp>
2020-04-19 15:58:19 +0000

    [ABC 094] C - Many Medians

--- c.cpp
+++ c.cpp
@@ -6,11 +6,29 @@ typedef long long ll;
 int main() {
   int n;
   cin >> n;
-  vector<int> b(n + 1, 0);
-  for (int i = 1; i < n; ++i) {
-    int a;
-    cin >> a;
-    b[a]++;
+  vector<int> x(n);
+  for (int i = 0; i < n; ++i) cin >> x[i];
+
+  vector<int> b = x;
+  sort(b.begin(), b.end());
+
+  for (int i = 0; i < n; ++i) {
+    if (x[i] < b[n / 2 - 1]) {
+      cout // << "1 "
+          << b[n / 2];
+    } else if (x[i] == b[n / 2 - 1]) {
+      cout // << "2 "
+          << b[n / 2];
+    } else if (x[i] > b[n / 2 - 1] && x[i] < b[n / 2]) {
+      cout // << "3 "
+          << b[n / 2];
+    } else if (x[i] == b[n / 2]) {
+      cout // << "4 "
+          << b[n / 2 - 1];
+    } else if (x[i] > b[n / 2]) {
+      cout // << "5 "
+          << b[n / 2 - 1];
+    }
+    cout << endl;
   }
-  for (int i = 1; i <= n; ++i) { cout << b[i] << endl; }
 }
