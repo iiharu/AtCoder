d0385379e27f8250cb91921a475967d916e7161b
iiharu <iiharu@alumni.tus.ac.jp>
2020-04-21 21:30:48 +0900

    [AGC 017] A - Biscuits
    c[0] := count({a \in A | A % 2 == 0})
    c[1] := count({a \in A | A % 2 == 1})
    
    A)
    c[0] から任意個数を選択する組み合わせの数
    
    B.0) P = 0
    c[1] から偶数個数を選択する組み合わせの数
    
    B.1) P = 1
    c[1] から奇数個数を選択する組み合わせの数
    
    答え: A * B

--- a.cpp
+++ a.cpp
@@ -3,8 +3,38 @@
 using namespace std;
 typedef long long ll;
 
+#include <bits/stdc++.h>
+
+static map<pair<ll, ll>, ll> mem;
+ll combi(ll n, ll k) {
+  if (n == k || k == 0) {
+    return 1;
+  } else {
+    if (!mem.count({n, k})) { mem.insert({{n, k}, combi(n - 1, k - 1) + combi(n - 1, k)}); }
+    return mem.at({n, k});
+  }
+}
+
 int main() {
-  int n;
-  cin >> n;
-  cout << 2 * M_PI * (double)n << endl;
+  int n, p;
+  cin >> n >> p;
+  vector<int> a(n);
+  int c[2] = {0, 0};
+  for (auto &e : a) {
+    cin >> e;
+    e %= 2;
+    ++c[e];
+  }
+
+  ll num[2] = {0, 0};
+  // even
+  {
+    for (int i = 0; i <= c[0]; ++i) { num[0] += combi(c[0], i); }
+  }
+  // odd
+  {
+    for (int i = p; i <= c[1]; i += 2) { num[1] += combi(c[1], i); }
+  }
+
+  cout << num[0] * num[1] << endl;
 }
