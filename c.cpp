1c9a5df4b2f3609a6568c0f276052796f86c3d25
iiharu <iiharu@alumni.tus.ac.jp>
2020-04-22 20:40:51 +0900

    Update

--- c.cpp
+++ c.cpp
@@ -1,26 +1,34 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
-typedef long long ll;
+using ll = long long;
+using P = pair<int, int>;
 
-int main() {
-  int n;
-  cin >> n;
-  vector<int> x(n);
-  vector<int> l(n);
-  for (int i = 0; i < n; ++i) {
-    cin >> x[i];
-    l[i] = i;
+signed main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<bitset<10>> f(n, bitset<10>());
+
+  for (int i = 0; i < m; ++i) {
+    int a, b;
+    cin >> a >> b;
+    f[a - 1].set(b - 1);
+    f[b - 1].set(a - 1);
   }
 
-  sort(l.begin(), l.end(), [&x](const auto &a, const auto &b) { return x[a] < x[b]; });
-  // for (int i = 0; i < n; ++i) { cout << x[l[i]] << endl; }
-
   for (int i = 0; i < n; ++i) {
-    if (x[i] <= x[l[n / 2 - 1]]) {
-      cout << x[l[n / 2]] << endl;
-    } else {
-      cout << x[l[n / 2 - 1]] << endl;
+    bitset<10> bs(0);
+    for (int j = 0; j < n; ++j) {
+      if (f[i].test(j)) { bs |= f[j]; }
     }
+    bs.reset(i);
+    for (int j = 0; j < n; ++j) {
+      if (f[i].test(j)) bs.reset(j);
+    }
+
+    cout << bs.count() << endl;
   }
+
+  return 0;
 }
