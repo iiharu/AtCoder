17d9781ab3dc11db5c8c82bcd92494342151028a
iiharu <iiharu@alumni.tus.ac.jp>
2020-04-26 19:58:40 +0900

    ABC038 C

--- c.cpp
+++ c.cpp
@@ -7,26 +7,17 @@ using P = pair<int, int>;
 signed main() {
   int n;
   cin >> n;
-  vector<int> c(n - 1);
-  vector<int> s(n - 1);
-  vector<int> f(n - 1);
-  for (int i = 0; i < n - 1; ++i) cin >> c[i] >> s[i] >> f[i];
 
-  vector<int> t(n, 0);
+  vector<int> a(n);
+  for (auto &e : a) cin >> e;
 
-  for (int i = 0; i < n - 1; ++i) {
-    for (int j = 0; j <= i; ++j) {
-      int _t = t[j] - s[i];
-      int _s;
-      if (_t <= 0)
-        _s = s[i];
-      else if (_t % f[i] == 0)
-        _s = s[i] + _t;
-      else
-        _s = s[i] + _t + f[i] - (_t % f[i]);
-
-      t[j] = _s + c[i];
-    }
+  ll num = 0;
+  for (int l = 0, r = 0; l < n && r < n; ++l) {
+    while (r < n - 1 && a[r] < a[r + 1]) { ++r; }
+    //    cout << l << ", " << r << " " << r - l << endl;
+    num += r - l + 1;
+    if (r == l) ++r;
   }
-  for (int i = 0; i < n; ++i) cout << t[i] << endl;
+
+  cout << num << endl;
 }
