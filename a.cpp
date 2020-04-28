01d38a99e95e562a3d986beaf9f0aec0ec51ae92
iiharu <iiharu@alumni.tus.ac.jp>
2020-04-28 23:50:52 +0900

    ABC 146 A

--- a.cpp
+++ a.cpp
@@ -6,21 +6,21 @@ typedef long long ll;
 #include <bits/stdc++.h>
 
 int main() {
-  int n, p;
-  cin >> n >> p;
-  bool flag = false;
-  for (int i = 0; i < n; ++i) {
-    int a;
-    cin >> a;
-    flag |= (a % 2 == 1);
-  }
-
-  if (flag) {
-    cout << static_cast<ll>(pow(2, n - 1)) << endl;
-  } else {
-    if (p == 0)
-      cout << static_cast<ll>(pow(2, n)) << endl;
-    else
-      cout << 0 << endl;
-  }
+  string s;
+  cin >> s;
+  if (s == "SUN")
+    cout << 7;
+  else if (s == "MON")
+    cout << 6;
+  else if (s == "TUE")
+    cout << 5;
+  else if (s == "WED")
+    cout << 4;
+  else if (s == "THU")
+    cout << 3;
+  else if (s == "FRI")
+    cout << 2;
+  else if (s == "SAT")
+    cout << 1;
+  cout << endl;
 }
