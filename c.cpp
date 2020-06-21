bfe216ee32708df286a16e610e101a65b4f6d03d
iiharu <iiharu@alumni.tus.ac.jp>
2020-06-21 18:18:00 +0900

    ABC 049 C

--- c.cpp
+++ c.cpp
@@ -5,19 +5,64 @@ using ll = long long;
 using P = pair<int, int>;
 
 signed main() {
-  int n;
-  cin >> n;
 
-  vector<int> a(n);
-  for (auto &e : a) cin >> e;
+  string s;
+  cin >> s;
 
-  ll num = 0;
-  for (int l = 0, r = 0; l < n && r < n; ++l) {
-    while (r < n - 1 && a[r] < a[r + 1]) { ++r; }
-    //    cout << l << ", " << r << " " << r - l << endl;
-    num += r - l + 1;
-    if (r == l) ++r;
+  auto it = s.rbegin();
+
+  while (true) {
+
+    bool flag = true;
+    if (it + 5 > s.rend()) { flag = false; }
+
+    if (!flag) {
+      cout << "NO" << endl;
+      return 0;
+    }
+
+    switch (*it) {
+    case 'm':
+      if (*(it + 1) == 'a' && *(it + 2) == 'e' && *(it + 3) == 'r' && *(it + 4) == 'd') {
+        it += 5;
+      } else {
+        flag = false;
+      }
+      break;
+    case 'e':
+      if (*(it + 1) == 's' && *(it + 2) == 'a' && *(it + 3) == 'r' && *(it + 4) == 'e') {
+        it += 5;
+      } else {
+        flag = false;
+      }
+      break;
+    case 'r':
+      if (*(it + 1) == 'e') {
+        if (it + 6 > s.rend()) {
+          flag = false;
+        } else if (*(it + 2) == 's' && *(it + 3) == 'a' && *(it + 4) == 'r' && *(it + 5) == 'e') {
+          it += 6;
+        } else if (it + 7 > s.rend()) {
+          flag = false;
+        } else if (*(it + 2) == 'm' && *(it + 3) == 'a' && *(it + 4) == 'e' && *(it + 5) == 'r' && *(it + 6) == 'd') {
+          it += 7;
+        } else {
+          flag = false;
+        }
+      } else {
+        flag = false;
+      }
+      break;
+    default: flag = false; break;
+    }
+
+    if (!flag) {
+      cout << "NO" << endl;
+      return 0;
+    }
+    if (it == s.rend()) {
+      cout << "YES" << endl;
+      return 0;
+    }
   }
-
-  cout << num << endl;
 }
