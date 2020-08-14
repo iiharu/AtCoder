b8c3a151cecb95de5a34623229c2d9972a266faa
iiharu <iiharu@alumni.tus.ac.jp>
2020-08-14 20:58:18 +0900

    [ABC 091] C

--- c.cpp
+++ c.cpp
@@ -5,19 +5,24 @@ using ll = long long;
 using P = pair<int, int>;
 
 signed main() {
-  int n;
-  cin >> n;
-
-  vector<int> a(n);
-  for (auto &e : a) cin >> e;
-
   ll num = 0;
-  for (int l = 0, r = 0; l < n && r < n; ++l) {
-    while (r < n - 1 && a[r] < a[r + 1]) { ++r; }
-    //    cout << l << ", " << r << " " << r - l << endl;
-    num += r - l + 1;
-    if (r == l) ++r;
+  ll n, m;
+  cin >> n >> m;
+
+  if (n == m) {
+    if (n == 1) num = 1;
+    else if (n == 2) num = 0;
+  } else if (n == 1 || m == 1) {
+    num = max(n, m) - 2;
+  } else {
+    num = max((ll)0, (n - 2) * (m - 2));
   }
+
+  if (n == m && n == 1) num = 1;
+  else if (n == m && n == 2) num = 0;
+  // else if (n == m && n >= 3) num = (n - 2) * (m - 2);
+  else if (n == 1 || m == 1) num = max(n, m) - 2;
+  else num = (n - 2) * (m - 2);
 
   cout << num << endl;
 }
