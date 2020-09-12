2bcaf45f8d5bd788eb9f736a87bc16ccb0985406
iiharu <iiharu@alumni.tus.ac.jp>
2020-09-13 00:22:35 +0900

    clean dir

--- lib.hpp
+++
@@ -1,4 +0,0 @@
-
-
-long long gcd(long long a, long long b) { return b ? gcd(b, a % b) : a; }
-long long lcm(long long a, long long b) { return a / gcd(a, b) * b; }
--- templates/gcd.cpp
+++
@@ -1,2 +0,0 @@
-template <typename T> gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
-template <typename T> lcm(T a, T b) { return a / gcd(a, b) * b; }
